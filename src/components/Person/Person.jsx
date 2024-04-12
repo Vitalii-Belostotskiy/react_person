f5c54035db1b800c4fb4bf3ec12a60a1ea3b1071
Vitalii Bilostotskyi <belostotskiyvitalii84@gmail.com>
2024-04-12 13:59:57 +0300

    working on mistakes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,13 +3,13 @@ export const Person = ({ person }) => (
     <h2 className="Person__name">My name is {person.name}</h2>
     {person.age > 0 && <p className="Person__age">{`I am ${person.age}`}</p>}
     <p className="Person__partner">
-      {person.sex === 'm' && person.isMarried === true
-        ? `${person.partnerName} is my wife`
-        : ''}
-      {person.sex === 'f' && person.isMarried === true
-        ? `${person.partnerName} is my husband`
-        : ''}
-      {person.isMarried === false ? 'I am not married' : ''}
+      {person.sex === 'm' &&
+        person.isMarried &&
+        `${person.partnerName} is my wife`}
+      {person.sex === 'f' &&
+        person.isMarried &&
+        `${person.partnerName} is my husband`}
+      {!person.isMarried && 'I am not married'}
     </p>
   </section>
 );
